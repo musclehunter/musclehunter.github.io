71cf449f48e2f53e96f5b669f6aa209dec66f888
Masanobu Sato <sato@masanobu.jp>
2025-05-29 22:57:14 +0900

    Vue3とTailwindCSSでウェブサイトを刷新。佐藤正伸（マッスルハンター）の個人サイトとしてリニューアル

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // GitHub Pages向けの相対パス設定
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
